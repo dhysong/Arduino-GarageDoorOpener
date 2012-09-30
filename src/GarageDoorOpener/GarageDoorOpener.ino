f33f83cdfb096d5d1d95921538f0c5a743cd0e4c
dhysong-nci <dhysong@nci.com>
2012-09-30 14:37:19 -0400

    added rf receiver

--- src/GarageDoorOpener/GarageDoorOpener.ino
+++ src/GarageDoorOpener/GarageDoorOpener.ino
@@ -1,5 +1,7 @@
+
 #include <SPI.h>
 #include <Ethernet.h>
+#include <VirtualWire.h>
 #define maxLength 25
 
 // Enter a MAC address and IP address for your controller below.
@@ -24,19 +26,32 @@ void setup() {
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
   
+  // Initialise the IO and ISR
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2400);	 // Bits per sec
+  vw_set_rx_pin(6);           // We will be receiving on pin 7 (Mega) ie the RX pin from the module connects to this pin. 
+
+  vw_rx_start();       // Start the receiver PLL running
+  
   //Connect to opener
   pinMode(triggerPin1, OUTPUT);
   digitalWrite(triggerPin1, HIGH);
   pinMode(triggerPin2, OUTPUT);
   digitalWrite(triggerPin2, HIGH);
   Serial.println("*** Ready.");
+  
 }
 
-void loop() {
+void loop() {  
+  
+   uint8_t buf[VW_MAX_MESSAGE_LEN];
+   uint8_t buflen = VW_MAX_MESSAGE_LEN;
+    
   // listen for incoming clients
   EthernetClient client = server.available();
   if (client) {
-    Serial.println("new client");
+    Serial.println("new client");    
+    
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
@@ -52,37 +67,44 @@ void loop() {
         if (c == '\n' && currentLineIsBlank) {
           // send a standard http response header
           client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connnection: close");
+          client.println("Content-Type: application/json; charset=utf-8");
+          client.println("Connnection: Keep-Alive");
           client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<head>");
-          client.println("<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js\"></script>");
-          client.println("</head>");
-          client.println("<body>");
-          client.println("<div style=\"width:300px; height: 400px; background-color: #000; -moz-border-radius: 15px; border-radius: 15px;\">");
-          client.println("<div onclick=\"document.location=$(location).attr('pathname')+'?1';\" style=\"cursor: pointer; margin-left: 12px; margin-top: 10px; float: left; width: 132px; height: 200px; background-color: #CCC; font-size: 48pt; text-align: center; -moz-border-radius: 5px; border-radius: 5px; font-family: arial\">I</div>");
-          client.println("<div onclick=\"document.location=$(location).attr('pathname')+'?2';\" style=\"cursor: pointer; margin-left: 12px; margin-top: 10px; float: left; width: 132px; height: 200px; background-color: #CCC; font-size: 48pt; text-align: center; -moz-border-radius: 5px; border-radius: 5px; font-family: arial\">II</div>");
-          client.println("</div>");
-          client.println("</div>");
-          client.println("</body>");
-          client.println("</html>");
 
-          
-          if (inString.indexOf("?1") > -1) {
-              Serial.println("*** Opening door1.");
+          if (vw_get_message(buf, &buflen)) // Non-blocking
+          {
+      	        int i;
+      
+        	// Message with a good checksum received, dump it.
+        	Serial.print("Distance: ");
+        	
+        	for (i = 0; i < buflen; i++)
+        	{
+        	    Serial.write(buf[i]);
+        	    Serial.print(" ");
+        	}
+        	Serial.println("");
+          }
+                   
+          if (inString.indexOf("?door=1") > -1) {
+              Serial.println("*** Triggering door1.");
               digitalWrite(triggerPin1, LOW);
               delay(1000);
               digitalWrite(triggerPin1, HIGH);
               Serial.println("*** Complete.");
+              client.println("{\"Status\": \"Open\", \"Door\": \"1\"}");
           }
-          else if (inString.indexOf("?2") > -1) {
-              Serial.println("*** Opening door2.");
+          else if (inString.indexOf("?door=2") > -1) {
+              Serial.println("*** Triggering door2.");
               digitalWrite(triggerPin2, LOW);
               delay(1000);
               digitalWrite(triggerPin2, HIGH);
               Serial.println("*** Complete.");
+              client.println("{\"Status\": \"Open\", \"Door\": \"2\"}");
+          }
+          else{
+              Serial.println("*** Trigger not found.");
+              client.println("{\"Status\": \"Ready\"}");
           }
           
           break;
